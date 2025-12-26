24ebed6c0e119e98a3ccec001f588d04ce3dbcf9
Su <suelagozz@gmail.com>
2025-12-26 15:35:34 +0300

    Implement encryption and decryption functions
    Added cryptographic functions for encryption and decryption using a keystream.

--- encrypt.c
+++ encrypt.c
@@ -1,17 +1,28 @@
 #include "encrypt.h"
+#include <string.h>
+#include <stdint.h>
 
-void encrypt(char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        str[i] = str[i] + 2;
-        i++;
-    }
+static uint32_t state = 0;
+
+void init_crypto(const char *master) {
+    // master password'tan pseudo key üret
+    for (int i = 0; master[i]; i++)
+        state = state * 131 + master[i];
 }
 
-void decrypt(char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        str[i] = str[i] - 2;  
-        i++;
-    }
+static uint8_t keystream() {
+    // xorshift PRNG
+    state ^= state << 13;
+    state ^= state >> 17;
+    state ^= state << 5;
+    return (uint8_t)(state & 0xFF);
+}
+
+void encrypt_string(char *data) {
+    for (int i = 0; data[i]; i++)
+        data[i] ^= keystream();
+}
+
+void decrypt_string(char *data) {
+    encrypt_string(data); // XOR symmetric
 }
