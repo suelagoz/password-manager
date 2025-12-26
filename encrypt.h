f9ba1270cf8b88645325ffd64e76c062c1d2b1c1
Su <suelagozz@gmail.com>
2025-12-26 15:34:08 +0300

    Add key length and update encryption functions

--- encrypt.h
+++ encrypt.h
@@ -1,7 +1,11 @@
 #ifndef ENCRYPT_H
 #define ENCRYPT_H
 
-void encrypt(char *str);
-void decrypt(char *str);
+#define KEY_LEN 32
+
+void derive_key(const char *master, unsigned char *key);
+
+int encrypt_string(const char *plaintext, unsigned char *ciphertext);
+int decrypt_string(const unsigned char *ciphertext, char *plaintext);
 
 #endif
