b50cd054b730056a769f4e59921b8bda5ad888eb
Su <suelagozz@gmail.com>
2025-12-26 15:34:48 +0300

    Refactor encryption functions and add init_crypto

--- encrypt.h
+++ encrypt.h
@@ -1,11 +1,9 @@
 #ifndef ENCRYPT_H
 #define ENCRYPT_H
 
-#define KEY_LEN 32
+void init_crypto(const char *master);
 
-void derive_key(const char *master, unsigned char *key);
-
-int encrypt_string(const char *plaintext, unsigned char *ciphertext);
-int decrypt_string(const unsigned char *ciphertext, char *plaintext);
+void encrypt_string(char *data);
+void decrypt_string(char *data);
 
 #endif
